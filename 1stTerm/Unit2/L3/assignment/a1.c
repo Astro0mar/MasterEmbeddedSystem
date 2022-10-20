7e6ed332f288f6750dd3fac35afc13049038d41b
Omar Mahmoud <astro0marfly@gmail.com>
2022-10-21 00:43:16 +0200

    trying to run using astronvim

--- 1stTerm/Unit2/L3/assignment/a1.c
+++ 1stTerm/Unit2/L3/assignment/a1.c
@@ -2,5 +2,8 @@
 int main() {
    // printf() displays the string inside quotation
    printf("Hello, World!");
+
+printf("hi my name is Omar\n");
+
    return 0;
 }
---
+++ 1stTerm/Unit2/L3/assignment/omar.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main ()
+{
+ int x=5;
+std::cout << "message" << std::endl;
+
+  return 0;
+}
