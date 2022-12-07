54881dd95be4323e1955c0556fe1e379da13f021
root <root@MSI.localdomain>
2022-12-07 11:49:44 +0200

    asi1ex1

--- 1stTerm/Unit2/L3/assignment/a1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
-
-printf("hi my name is Omar\n");
-
-   return 0;
-}
--- 1stTerm/Unit2/L3/assignment/omar.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main ()
-{
- int x=5;
-std::cout << "message" << std::endl;
-
-  return 0;
-}
